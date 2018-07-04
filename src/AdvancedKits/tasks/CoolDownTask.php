0268e342364dda76d77d6b8b708c5c4626a13de1
QuiltedTuba2798 <40707834+QuiltedTuba2798@users.noreply.github.com>
2018-07-04 17:13:36 -0400

    Create CoolDownTask.php

---
+++ src/AdvancedKits/tasks/CoolDownTask.php
@@ -0,0 +1,15 @@
+<?php
+namespace AdvancedKits\tasks;
+use AdvancedKits\Main;
+use pocketmine\scheduler\Task;
+class CoolDownTask extends Task{
+    private $plugin;
+    public function __construct(Main $plugin){
+        $this->plugin = $plugin;
+    }
+    public function onRun(int $tick){
+        foreach($this->plugin->kits as $kit){
+            $kit->processCoolDown();
+        }
+    }
+}
